6813eae0d9399ce1a56957c9ca82e6ffe4276960
Gvo3d <denis.yakimov89@gmail.com>
2015-11-08 18:36:25 +0200

    Initial commit

---
+++ src/Arithmetic_mean/Application.java
@@ -0,0 +1,19 @@
+package Arithmetic_mean;
+
+import java.util.Scanner;
+
+/**
+ * Created by Gvozd on 08.11.2015.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Arithetic mean");
+        System.out.println("For Hillel Java Courses by Yakimov Denis");
+        System.out.println("Enter any count of numbers. After that - input \"e\" for result and program end.");
+        long SumOfAllNumbers[];
+
+        Scanner InputScanner = new Scanner(System.in);
+        
+
+    }
+}
