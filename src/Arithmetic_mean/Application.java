ea354b8c80b42155e03da0d22b1c1c149ea780b4
Gvo3d <denis.yakimov89@gmail.com>
2015-11-08 18:56:34 +0200

    Stable, with no errors.

--- src/Arithmetic_mean/Application.java
+++ src/Arithmetic_mean/Application.java
@@ -2,18 +2,30 @@ package Arithmetic_mean;
 
 import java.util.Scanner;
 
-/**
- * Created by Gvozd on 08.11.2015.
- */
 public class Application {
     public static void main(String[] args) {
         System.out.println("Arithetic mean");
         System.out.println("For Hillel Java Courses by Yakimov Denis");
         System.out.println("Enter any count of numbers. After that - input \"e\" for result and program end.");
-        long SumOfAllNumbers[];
+        long SumOfAllNumbers = 0;
+        int NumberCount = 0;
+        String EndFlag;
 
-        Scanner InputScanner = new Scanner(System.in);
-        
+        while (true) {
+            Scanner InputScanner = new Scanner(System.in);
+            EndFlag = InputScanner.nextLine();
+            EndFlag=EndFlag.toUpperCase();
+            if (InputScanner.hasNextLong()) {
+                SumOfAllNumbers = SumOfAllNumbers + InputScanner.nextLong();
+                NumberCount++;
+            }
+            if (EndFlag.equals("E")) {
+                System.out.println("The sum is: " + SumOfAllNumbers);
+                System.out.println("The number of all summands is: " + NumberCount);
+                System.out.println("The arithmetic mean is: " + SumOfAllNumbers / NumberCount);
+                System.exit(0);
+            }
+        }
 
     }
 }
